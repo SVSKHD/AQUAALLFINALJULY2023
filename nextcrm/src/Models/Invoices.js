7de84bff639ee095d5c0aa630c954769c7be2a6f
hithesh svsk <62789585+SVSKHD@users.noreply.github.com>
2023-08-05 18:48:10 +0530

    invoice

--- nextcrm/src/Models/Invoices.js
+++ nextcrm/src/Models/Invoices.js
@@ -1,81 +1,81 @@
 import mongoose from "mongoose";
 
-
 const AquaInvoiceSchema = new mongoose.Schema(
-    {
-        invoiceNo: {
-            type: String
-        },
-        customerDetails: {
-            name: {
-                type: String
-            },
-            phone: {
-                type: Number,
-
-            },
-            email: {
-                type: String
-            },
-            address: {
-                type: String
-            },
-        },
-        gst: {
-            type: Boolean
-        },
-        gstDetails: {
-            gstName: {
-                type: String
-            },
-            gstPhone: {
-                type: Number
-            },
-            gstEmail: {
-                type: String
-            },
-            gstAddress: {
-                type: String
-            },
-        },
-        products: [
-            {
-                productName: {
-                    type: String,
-                },
-                productQuantity: {
-                    type: Number
-                },
-                productPrice: {
-                    type: Number
-                },
-                productSerialNo: {
-                    type: Number
-                }
-            },
-        ],
-        trasnport: [
-            {
-                deliveredBy: {
-                    type: String
-                },
-                deliveryDate: {
-                    type: String
-                }
-            }
-        ],
-        paidStatus: {
-            type: String
-        },
-        paytmentType: {
-            type: String
-        }
+  {
+    invoiceNo: {
+      type: String,
     },
-    {
-        timestamps: true,
-    }
+    date: {
+      type: String,
+    },
+    customerDetails: {
+      name: {
+        type: String,
+      },
+      phone: {
+        type: Number,
+      },
+      email: {
+        type: String,
+      },
+      address: {
+        type: String,
+      },
+    },
+    gst: {
+      type: Boolean,
+    },
+    gstDetails: {
+      gstName: {
+        type: String,
+      },
+      gstPhone: {
+        type: Number,
+      },
+      gstEmail: {
+        type: String,
+      },
+      gstAddress: {
+        type: String,
+      },
+    },
+    products: [
+      {
+        productName: {
+          type: String,
+        },
+        productQuantity: {
+          type: Number,
+        },
+        productPrice: {
+          type: Number,
+        },
+        productSerialNo: {
+          type: String,
+        },
+      },
+    ],
+    trasnport: {
+      deliveredBy: {
+        type: String,
+      },
+      deliveryDate: {
+        type: String,
+      },
+    },
+    paidStatus: {
+      type: String,
+    },
+    paytmentType: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-const Cart = mongoose.model("AquaInvoices", AquaInvoiceSchema);
+const AquaInvoices =
+  mongoose.models.AquaInvoices || mongoose.model("AquaInvoices", AquaInvoiceSchema);
 
-export default Cart;
+export default AquaInvoices;
--- nextcrm/src/pages/api/invoice.js
+++ nextcrm/src/pages/api/invoice.js
@@ -1,16 +1,40 @@
 import { createRouter } from "next-connect";
 import db from "@/middleware/db";
+import moment from "moment";
+import { nanoid } from "nanoid";
+import AquaInvoices from "../../Models/Invoices";
 
 const router = createRouter();
 
+const invoiceId = nanoid(5);
+const date = moment(new Date()).format("DD/MM/YYYY");
+
 router.post(async (req, res) => {
   try {
-    db.connectDb()
-    const body = req.body
-    res.status(200).json(body)
-    db.disconnectDb()
+    db.connectDb();
+    const body = req.body;
+    body.invoiceNo = `AQB-${date}-${invoiceId}`;
+    body.date = date;
+    const invoice = new AquaInvoices(body);
+    await invoice.save();
+    res.status(200).json(invoice);
+    db.disconnectDb();
   } catch (error) {
-    
+    res.status(400).json({ message: error.message });
+  }
+});
+
+router.put(async (req, res) => {
+  try {
+    const body = req.body;
+    const { invoiceNo } = body;
+    console.log(body);
+    if (invoiceNo) {
+      let updated = AquaInvoices.findByIdAndUpdate(id,body)
+      res.json(updated);
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
   }
 });
 
